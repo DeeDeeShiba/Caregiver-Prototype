e7f7fdb0954ea87f24a5464c51ddace0a52e0914
david <a.davidromero@icloud.com>
2024-06-12 15:41:03 -0400

    heat map works, will expand to work with a grandma emoji marker.

--- App.js
+++ App.js
@@ -1,111 +1,6 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
-
-
-
-// App.js
-// import { APIProvider } from '@vis.gl/react-google-maps';
-// import { createRoot } from "react-dom/client";
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-// import React from 'react';
-// import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-// import { firebase } from 'firebase/app';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <MapView
-//         provider={PROVIDER_GOOGLE}
-//         style={styles.map}
-//         initialRegion={{
-//           latitude: 37.78825,
-//           longitude: -122.4324,
-//           latitudeDelta: 0.0922,
-//           longitudeDelta: 0.0421,
-//         }}
-//       />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   map: {
-//     ...StyleSheet.absoluteFillObject,
-//   },
-// });
-
-//"AIzaSyBSxFbm2MTSWjPZW7qSIcJYsntXB0JH0AU"
-
-
-// import React, { useEffect, useState } from 'react';
-// import MapView, {Marker} from 'react-native-maps';
-// import { StyleSheet, View } from 'react-native';
-// import { db } from 'firebase/app';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <MapView
-//         style={styles.map}
-//         initialRegion={{
-//           latitude: 43.468601,
-//           longitude: -79.700432,
-//           latitudeDelta: 0.0922,
-//           longitudeDelta: 0.0421,
-//         }}
-//         >
-//           <Marker
-//           coordinate={{
-//             latitude: 43.468601,
-//             longitude: -79.700432,
-//           }}
-//           title="My Marker"
-//           description="This is a description of the marker"
-//         />
-//         </MapView>
-      
-//     </View>
-
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   map: {
-//     width: '100%',
-//     height: '100%',
-//   },
-// });
-
+//App.js
 import React, { useEffect, useState } from 'react';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Heatmap } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import { db } from './firebase';
 import { ref, onValue } from 'firebase/database';
@@ -158,6 +53,19 @@ export default function App() {
             description={`Lat: ${location.latitude}, Lng: ${location.longitude}`}
           />
         ))}
+        <Heatmap
+          points={locations.map(location => ({
+            latitude: location.latitude,
+            longitude: location.longitude,
+            weight: 1, // You can adjust the weight as needed
+          }))}
+          opacity={0.6}
+          radius={20}
+          maxIntensity={100}
+          gradientSmoothing={10}
+          heatmapMode="POINTS_DENSITY"
+        />
+
       </MapView>
     </View>
   );
